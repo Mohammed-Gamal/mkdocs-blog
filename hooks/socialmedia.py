20eb2162403570ad73d2aa0a51e656da53987a3a
Mohamed Gamal <1Mohamed.Gamal54@gmail.com>
2024-11-23 18:11:45 +0200

    major fixes

--- hooks/socialmedia.py
+++ hooks/socialmedia.py
@@ -13,11 +13,17 @@ def on_page_markdown(markdown, **kwargs):
     if not include.match(page.url):
         return markdown
 
-    page_url = config.site_url+page.url
-    page_title = urllib.parse.quote(page.title+'\n')
+    page_url = config.site_url + page.url
+    page_title = urllib.parse.quote(page.title)
 
     return markdown + dedent(f"""
-    [Share on :simple-x:]({x_intent}?text={page_title}&url={page_url}){{ .md-button }}
-    [Share on :simple-facebook:]({fb_sharer}?u={page_url}){{ .md-button }}
-    [Share on :simple-linkedin:]({in_share}?mini=true&url={page_url}&title={page_title}){{ .md-button }}
+    <a href="https://x.com/intent/tweet?text={page_title}&url={urllib.parse.quote(page_url)}" target="_blank" class="md-button">
+        <i class="fab fa-twitter" style="margin-right: 5px;"></i> Share on Twitter
+    </a>
+    <a href="https://www.facebook.com/sharer/sharer.php?u={urllib.parse.quote(page_url)}" target="_blank" class="md-button">
+        <i class="fab fa-facebook" style="margin-right: 5px;"></i> Share on Facebook
+    </a>
+    <a href="https://www.linkedin.com/shareArticle?mini=true&url={urllib.parse.quote(page_url)}&title={page_title}" target="_blank" class="md-button">
+        <i class="fab fa-linkedin" style="margin-right: 5px;"></i> Share on LinkedIn
+    </a>
     """)
